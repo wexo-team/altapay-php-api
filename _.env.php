ee258de04829df86dd4e4d4212dbb809e383b77e
Martin Aarhof <martin.aarhof@gmail.com>
2016-06-08 15:05:20 +0200

    deleted _env.php

--- _.env.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_ENV['USERNAME'] = 'maa@scandesigns.dk';
-$_ENV['PASSWORD'] = 'u3Htbc=Uc?w=';
-$_ENV['BASEURL'] = '';
-$_ENV['TERMINAL'] = 'Scandesigns Test Terminal';
-$_ENV['NUMBER_OF_TERMINALS'] = 3;
