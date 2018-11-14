27e861e22978073f20495bccedcf880947115f4c
SimionZ <sz-altapay-com@users.noreply.github.com>
2018-11-14 09:58:32 +0100

    Update the allowed languages
    Sync the supported languages with the ones from the payment gateway

--- src/Types/LanguageTypes.php
+++ src/Types/LanguageTypes.php
@@ -28,13 +28,14 @@ class LanguageTypes implements TypeInterface
 
     /**
      * Allowed languages
-     *
+     * nb, nn will be converted to no.
+     * ee will be converted to et
      * @var array
      */
     private static $languages = [
-        'cs', 'da', 'de', 'en', 'es', 'fi', 'fr', 'ja',
-        'lt', 'nl', 'no', 'pl', 'sv','th', 'tr', 'zh',
-        'et', 'it', 'pt', 'eu'
+	    'cs', 'da', 'de', 'en', 'es', 'fi', 'fr', 'ja',
+	    'lt', 'nl', 'no', 'nb', 'nn', 'pl', 'sv','th', 'tr', 'zh',
+	    'et', 'ee', 'it', 'pt', 'eu'
     ];
 
     /**
